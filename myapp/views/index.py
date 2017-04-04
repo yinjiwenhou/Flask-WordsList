71852ad45de59daddffab8867f60595bbe285653
Tom Hills <cugzhangtao@163.com>
2017-04-04 19:23:35 +0800

    test to use the ajax to query the translates

--- myapp/views/index.py
+++ myapp/views/index.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, flash, redirect, url_for
+from flask import Blueprint, render_template, flash, redirect, url_for, jsonify, request
 from ..modules import Words, Translates
 from .. import db
 from flask_login import login_required
@@ -9,7 +9,7 @@ index = Blueprint('index', __name__)
 @login_required
 def index_page():
 	return '<h2>Hello</h2>'
-
+'''
 @index.route('/words-list')
 def query_all():
 	word_dict = {}
@@ -19,6 +19,12 @@ def query_all():
 		word_dict[word] = trans
 
 	return render_template('index.html', words=word_dict)
+'''
+@index.route('/words-list')
+def query_all():
+	words_list = Words.query.all()
+	return render_template('index.html', words=words_list)
+
 
 @index.route('/delete/<int:id>')
 def delete_word(id):	
@@ -36,3 +42,11 @@ def delete_word(id):
 @index.route('/page-test')
 def show_page():
 	return render_template('page.html')
+
+@index.route('/detail/<int:id>', methods=['GET',])
+def word_detail(id):
+	tasks = []
+	trans = Translates.query.filter_by(word_id=id).all()
+	for translate in trans:
+		tasks.append(translate.Translate)
+	return jsonify({'task':tasks})
